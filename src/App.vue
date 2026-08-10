7dad7b0576b839137e60923baadd5de72a740bdb
ArthurVendra <vendraminiarthur2008@gmail.com>
2026-08-10 20:44:35 -0300

    New: Primeira página de login, com rules e refs dos campos email e password

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,13 @@
+<script lang="ts" setup>
+  import login from './pages/login.vue';
+</script>
+
+
 <template>
   <v-app>
     <v-main>
-      <HelloWorld />
+      <login/>
     </v-main>
-    <v-btn
-      class="ma-2"
-      icon="mdi-theme-light-dark"
-      location="top right"
-      position="absolute"
-      @click="$vuetify.theme.cycle()"
-    />
   </v-app>
 </template>
 
-<script lang="ts" setup>
-  import HelloWorld from '@/components/HelloWorld.vue'
-</script>
---
+++ src/pages/login.vue
@@ -0,0 +1,41 @@
+<script lang="ts" setup>
+    import { ref } from 'vue';
+
+  const email = ref<string>('')
+  const password = ref<string>('')
+
+  const emailRules = [
+    (value: boolean) => {
+        if (value) return true
+        return 'You must enter an email'
+    }
+  ]
+
+    const passwordRules = [
+    (value: boolean) => {
+        if (value) return true
+        return 'You must enter an password'
+    }
+  ]
+</script>
+
+
+<template>
+  <v-app>
+    <v-main>
+        <v-container class="fill-height d-flex justify-center align-center">
+            <v-card class="mx-auto" prepend-icon="$vuetify" subtitle="Log in" width="400"> 
+                <template v-slot:title> <span class="font-weight-black">Welcome to Axis App</span> </template>
+
+                <v-form @submit.prevent>
+                <v-text-field label="Email" v-model="email" :rules="emailRules"></v-text-field>
+                <v-text-field label="Password" v-model="password" :rules="passwordRules"></v-text-field>
+
+                <v-btn text="" type="submit">Submit</v-btn>
+                </v-form>
+            </v-card>
+        </v-container>
+    </v-main>
+  </v-app>
+</template>
+
