c23194d39b81e86182c1fda6e321d15d05536f28
ArthurVendra <vendraminiarthur2008@gmail.com>
2026-08-10 21:09:04 -0300

    New: Adiciona rules para senhas com menos de 5 digitos, e emails invalidos + ajustes de UI no form de login

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,22 +1,34 @@
 <script lang="ts" setup>
     import { ref } from 'vue';
 
-  const email = ref<string>('')
-  const password = ref<string>('')
+    const email = ref<string>('')
+    const password = ref<string>('')
 
-  const emailRules = [
-    (value: boolean) => {
-        if (value) return true
-        return 'You must enter an email'
-    }
-  ]
+    const emailRules = [
+        (value: boolean) => {
+            return value ? true : 'You must enter an email'
+        },
+
+        (value: string) => {
+            const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+
+            return regex.test(value) ? true : 'Invalid email'
+        }
+    ]
 
     const passwordRules = [
-    (value: boolean) => {
-        if (value) return true
-        return 'You must enter an password'
-    }
-  ]
+        (value: boolean) => {
+            return value ? true : 'You must enter an password'
+        },
+
+        (value: string) => {
+            return value.length >= 6 ? true : 'Your password must have more than 5 digits'
+        }
+    ]
+
+  function login() {
+    alert(`${email.value}  ${password.value}`)
+  }
 </script>
 
 
@@ -24,15 +36,16 @@
   <v-app>
     <v-main>
         <v-container class="fill-height d-flex justify-center align-center">
-            <v-card class="mx-auto" prepend-icon="$vuetify" subtitle="Log in" width="400"> 
+            <v-card class="mx-auto d-flex align-center flex-column pa-2" prepend-icon="$vuetify" width="400"> 
                 <template v-slot:title> <span class="font-weight-black">Welcome to Axis App</span> </template>
+                <v-card-text class="w-100 mt-5">
+                    <v-form @submit.prevent='login'>
+                    <v-text-field label="Email" v-model="email" :rules="emailRules"></v-text-field>
+                    <v-text-field label="Password" v-model="password" :rules="passwordRules"></v-text-field>
 
-                <v-form @submit.prevent>
-                <v-text-field label="Email" v-model="email" :rules="emailRules"></v-text-field>
-                <v-text-field label="Password" v-model="password" :rules="passwordRules"></v-text-field>
-
-                <v-btn text="" type="submit">Submit</v-btn>
-                </v-form>
+                    <v-btn text="" type="submit" class="w-100 mt-5">Submit</v-btn>
+                    </v-form>
+                </v-card-text>
             </v-card>
         </v-container>
     </v-main>
