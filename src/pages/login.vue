9ab7f99a12c2220b1850fd08bb5426d246ccfc71
ArthurVendra <vendraminiarthur2008@gmail.com>
2026-08-10 21:38:29 -0300

    New: Adicionado validação do formulário

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -4,6 +4,8 @@
     const email = ref<string>('')
     const password = ref<string>('')
 
+    const form = ref()
+
     const emailRules = [
         (value: boolean) => {
             return value ? true : 'You must enter an email'
@@ -26,24 +28,32 @@
         }
     ]
 
-  function login() {
-    alert(`${email.value}  ${password.value}`)
-  }
+    async function login() {
+        const { valid } = await form.value.validate()
+
+        if (!valid) {
+            return
+        }
+
+        alert('Login realizado com sucesso')
+        email.value = ''
+        password.value = ''
+    }
 </script>
 
 
 <template>
-  <v-app>
+  <v-app >
     <v-main>
         <v-container class="fill-height d-flex justify-center align-center">
-            <v-card class="mx-auto d-flex align-center flex-column pa-2" prepend-icon="$vuetify" width="400"> 
+            <v-card class="mx-auto d-flex align-center flex-column pa-2" prepend-icon="$vuetify" width="400" variant="tonal" color="indigo"> 
                 <template v-slot:title> <span class="font-weight-black">Welcome to Axis App</span> </template>
                 <v-card-text class="w-100 mt-5">
-                    <v-form @submit.prevent='login'>
+                    <v-form @submit.prevent='login' ref="form">
                     <v-text-field label="Email" v-model="email" :rules="emailRules"></v-text-field>
                     <v-text-field label="Password" v-model="password" :rules="passwordRules"></v-text-field>
 
-                    <v-btn text="" type="submit" class="w-100 mt-5">Submit</v-btn>
+                    <v-btn type="submit" class="w-100 mt-5" variant="outlined">Submit</v-btn>
                     </v-form>
                 </v-card-text>
             </v-card>
