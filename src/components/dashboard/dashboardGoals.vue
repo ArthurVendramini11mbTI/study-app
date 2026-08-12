1fbc450f2d11137f6b71812ebfef711916a7a97e
ArthurVendra <vendraminiarthur2008@gmail.com>
2026-08-11 21:18:23 -0300

    New: Separação das seções no dashboard

---
+++ src/components/dashboard/dashboardGoals.vue
@@ -0,0 +1,11 @@
+<script lang="ts"></script>
+
+<template>
+    <v-main class="border"></v-main>
+</template>
+
+<style>
+.border{
+    border: 1px solid red;
+}
+</style>
--- src/pages/dashboard.vue
+++ src/pages/dashboard.vue
@@ -1,8 +1,31 @@
-<script lang="ts">
+<script setup lang="ts">
+import DashboardCalendar from '@/components/dashboard/dashboardCalendar.vue';
+import DashboardGoals from '@/components/dashboard/dashboardGoals.vue';
+import DashboardSideBar from '@/components/dashboard/dashboardSideBar.vue';
+import DashboardUpperBar from '@/components/dashboard/dashboardUpperBar.vue';
+
 </script>
 
 <template>
-    <v-main>
-        
-    </v-main>
+    <v-row no-gutters>
+        <v-col cols="3">
+            <DashboardSideBar />
+        </v-col>
+
+        <v-col cols="9">
+            <v-row>
+                <DashboardUpperBar />
+            </v-row>
+
+            <v-row no-gutters>
+                <v-col cols="7">
+                    <DashboardCalendar />
+                </v-col>
+
+                <v-col cols="5">
+                    <DashboardGoals />
+                </v-col>
+            </v-row>
+        </v-col>
+    </v-row>
 </template>
